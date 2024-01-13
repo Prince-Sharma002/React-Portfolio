ce71dd542dee3ba12bbb087d36805ffb80fcb53c
Prince-Sharma002 <prince12845sharma@gmail.com>
2024-01-13 19:03:14 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import Header from "./components/Header";
+import Header,{HeaderPhone} from "./components/Header";
 import Home from "./components/Home";
 import Work from "./components/Work";
 import Timeline from "./components/Timeline";
 import Services from "./components/Services";
 import Testimonails from "./components/Testimonails";
+import Contact from "./components/Contact";
+import Footer  from "./components/Footer.jsx";
+import { Toaster } from "react-hot-toast";
+import { useState } from "react";
 
 
 function App() {
+
+  const [menuOpen , setMenuOpen] = useState(false);
+  console.log(menuOpen);
   return (
 <>
-    <Header />
+    <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     <Home />
     <Work />
     <Timeline />
     <Services />
     <Testimonails />
+    <Contact />
+    <Footer />
+    <Toaster />
 </>
   );
 }
