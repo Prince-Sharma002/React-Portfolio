73ddbbe64c03bc623bc7322218f5dae487067251
Prince-Sharma002 <prince12845sharma@gmail.com>
2024-01-13 14:02:16 +0530

    updates

--- src/components/Testimonails.jsx
+++ src/components/Testimonails.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Testimonails = () => {
   return (
-    <div id='testimonails'>Testimonails</div>
+    <div id='testimonails'> 
+<h2>Testimonial</h2>
+
+<section>
+  <TestimonialCard
+    name={"Abhishek"}
+    feedback={"Your Teaching skills are so good."}
+  />
+
+  <TestimonialCard
+    name={"Ramzi"}
+    feedback={
+      "Wow what a portfolio, doesn't expected this to be on youtube!"
+    }
+  />
+
+  <TestimonialCard
+    name={"Sundar Pichai"}
+    feedback={"Amazing seems like you should the Google CEO"}
+  />
+</section>
+    </div>
   )
 }
 
